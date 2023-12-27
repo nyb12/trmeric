73ddd564be607271efc38eba9e08a96ba2ca1ffd
Chaitrali R <chaitrali_r@tekditechnologies.com>
2023-12-27 14:44:18 +0530

    Add Provider API

--- src/api/ApiCalls.jsx
+++ src/api/ApiCalls.jsx
@@ -75,3 +75,26 @@ export const getAllNotes = async (params = {}, header = {}) => {
     };
   }
 };
+
+
+export const addProvider = async (params = {}, header = {}, pid = 1) => {
+  let headers = {
+    ...header,
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
+  };
+  try {
+    const result = await post(`${baseUrl}/api/create/provider/${pid}/`, params, {
+      headers,
+    });
+    if (result.data) {
+      return result.data;
+    } else {
+      return {};
+    }
+  } catch ({ response, message }) {
+    return {
+      status: response?.status ? response?.status : 404,
+      error: response?.data?.message ? response?.data?.message : message,
+    };
+  }
+};
--- src/components/common/AccordionCard/AccordionCards.jsx
+++ src/components/common/AccordionCard/AccordionCards.jsx
@@ -100,43 +100,9 @@ export default function CustomizedAccordions() {
         </AccordionSummary>
         <AccordionDetails>
           <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-
-            {cardList < 3 ? <Box>
-              <TrmericCard />
             <TrmericCard />
             <TrmericCard />
-            </Box>  : 
-            <Box>
-              <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
-            </Box>
-            
-            
-            
-            
-            }
-           
+            <TrmericCard />
            
           </Box>
         </AccordionDetails>
